bbd20528cefd23dc0014acdd0e5c2a59c2832c8a
aisc <2658939136@qq.com>
2026-03-08 16:35:32 +0800

    更新

--- src/components/ConfigPanel.tsx
+++ src/components/ConfigPanel.tsx
@@ -133,7 +133,7 @@ export const ConfigPanel = () => {
                                 onClick={() => setIsConfigPanelOpen(false)}
                                 aria-label="关闭配置面板"
                             >
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L7.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" /></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" /></svg>
                             </button>
                         </div>
                     </div>
